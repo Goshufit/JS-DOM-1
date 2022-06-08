57158d41ff67005fbdcfba03eee3440e140159e7
Jean-Pierre Graham <91097729+Goshufit@users.noreply.github.com>
2022-06-07 22:58:57 -0400

    Add getElementById selector add getElementById selector

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-//console.dir(document);
+// Examine The Document Object //
 
-console.log(document.domain);
-console.log(document.URL);
-console.log(document.title);
+//console.dir(document);
+//console.log(document.domain);
+//console.log(document.URL);
+//console.log(document.title);
 //document.title = 123;
-console.log(document.doctype);
-console.log(document.head);
-console.log(document.body);
-console.log(document.all);
-console.log(document.all[10]);
-document.all[10].textContent = 'Hello';
+//console.log(document.doctype);
+//console.log(document.head);
+//console.log(document.body);
+//console.log(document.all);
+//console.log(document.all[10]);
+//document.all[10].textContent = 'Hello';
+//console.log(document.forms[0]);
+//console.log(document.links);
+//console.log(document.images);
+
+
+// getElementById //
+
+//console.log(document.getElementById('header-title'));
+let headerTitle = document.getElementById('header-title');
+let header = document.getElementById('main-header');
+ //headerTitle.textContent = 'Hello';
+// headerTitle.innerText = 'Goodbye';
+//headerTitle.innerHTML = '<h3>Hello</h3>';
+//headerTitle.style.borderBottom = 'solid 3px #000';
+
+
+//getElementsByClassName//
+
+let items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'Hello 2';
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = 'yellow';
