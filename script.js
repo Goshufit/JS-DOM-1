a034e9c52ded297e7c13e8b41ea620c1666eb879
Jean-Pierre Graham <91097729+Goshufit@users.noreply.github.com>
2022-06-09 11:54:57 -0400

    Add getElementByTagName

--- script.js
+++ script.js
@@ -19,9 +19,9 @@
 // getElementById //
 
 //console.log(document.getElementById('header-title'));
-let headerTitle = document.getElementById('header-title');
-let header = document.getElementById('main-header');
- //headerTitle.textContent = 'Hello';
+// let headerTitle = document.getElementById('header-title');
+// let header = document.getElementById('main-header');
+//headerTitle.textContent = 'Hello';
 // headerTitle.innerText = 'Goodbye';
 //headerTitle.innerHTML = '<h3>Hello</h3>';
 //headerTitle.style.borderBottom = 'solid 3px #000';
@@ -29,9 +29,52 @@ let header = document.getElementById('main-header');
 
 //getElementsByClassName//
 
-let items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent = 'Hello 2';
-items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'yellow';
+// let items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello 2';
+// items[1].style.fontWeight = 'bold';
+// items[1].style.backgroundColor = 'yellow';
+
+//Gives error when trying to select all list items and change there background //
+//so you must do a for loop to iterate through each list item and apply the styling//
+//items.style.backgroundColor = '#f4f4f4';
+
+// for(var i = 0; i < items.length; i++){
+//     items[i].style.backgroundColor = '#f4f4f4';
+// }
+
+
+//getElemntByTagName //
+
+// let li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = 'Hello 2';
+// li[1].style.fontWeight = 'bold';
+// li[1].style.backgroundColor = 'yellow';
+
+//Gives error when trying to select all list items and change there background //
+//so you must do a for loop to iterate through each list item and apply the styling//
+//items.style.backgroundColor = '#f4f4f4';
+
+// for(var i = 0; i < li.length; i++){
+//     li[i].style.backgroundColor = '#f4f4f4';
+// }
+
+
+//querySelector//
+
+let header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #ccc';
+
+let input = document.querySelector('input');
+input.value = 'Hello World';
+
+let submit = document.querySelector('input[type="submit"]');
+submit.value="SEND";
+
+let item = document.querySelector('.list-group-item');
+item.style.color = 'red';
+
+let lastItem = document.querySelector('.list-group-item:last');
